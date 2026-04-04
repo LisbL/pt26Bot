7aa52f18bcedbc2773182203ef1bfd351e2769da
LisbL <LisbethLepp12@gmail.com>
2026-04-04 18:27:16 +0300

    jar for testing initial bot

--- src/goofyGoober.java
+++ src/goofyGoober.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 public class goofyGoober {
     public static void main(String[] args) {
-        
+        String budget = args[0];
+
+        Scanner input = new Scanner(System.in);
+
+        //choosing a category
+        System.out.println("Video Games");
+
+        //unending cycle of betting
+        while (input.hasNextLine()) {
+            String vidInfo = input.nextLine();
+
+            if (vidInfo.startsWith("S ")) {
+                continue; // dont need the summary
+            }
+
+            //bidding
+            //testing purposes always bidding, start 1, max 10
+            System.out.println("1 5");
+
+            //processing result
+            if (input.hasNextLine()) {
+                String result = input.nextLine();
+            }
+        }
     }
 }
